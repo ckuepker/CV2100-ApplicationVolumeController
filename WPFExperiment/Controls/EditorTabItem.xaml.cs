6d75708f5ce65d0afd582b9b16c787b46ad30964
Christoph Küpker <christoph.kuepker@googlemail.com>
2016-11-23 16:47:13 +0100

    Missing Files from last commit

---
+++ WPFExperiment/Controls/EditorTabItem.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFExperiment.OM;
+
+namespace WPFExperiment.Controls
+{
+    /// <summary>
+    /// Interaction logic for EditorTab.xaml
+    /// </summary>
+    public partial class EditorTabItem : TabItem
+    {
+        public EditorTabItem(Profile p)
+        {
+            InitializeComponent();
+            this.Header = p.Name;
+        }
+    }
+}
