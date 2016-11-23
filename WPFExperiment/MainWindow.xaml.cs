015399043706c087365608178fc78e35f90a4436
Christoph Küpker <christoph.kuepker@googlemail.com>
2016-11-23 11:45:34 +0100

    Initial commit

---
+++ WPFExperiment/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFExperiment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(String.Format("{0}", input.Text));
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("New File");
+        }
+
+        private void RowDefinition_IsKeyboardFocusWithinChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WPFExperiment/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFExperiment.Data
+{
+    public class Person
+    {
+        public Person()
+        {
+            this._name = "Max Müller";
+            this._age = 47;
+        }
+
+        private String _name;
+        private int _age;
+
+        public String Name
+        {
+            get { return this._name; }
+        }
+        public int Age
+        {
+            get { return this._age; }
+        }
+
+
+    }
+}
